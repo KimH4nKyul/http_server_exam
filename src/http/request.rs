6632d3fd111055cca5b6f21996471c45ebb3f22a
KimH4nKyul <alskim93@kakao.com>
2023-02-20 18:20:03 +0900

    [Update] added Tcp Listener. TODO: convert byte array to Request type.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,18 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>, 
     method: Method
+}
+
+impl Request {
+    pub fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        Self
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    // TODO
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;  // stream에서 read() 메서드를 사용하기 위해 Read 트레이트를 불러온다. 
 
 pub struct Server {
     addr: String,
@@ -13,6 +14,26 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on: {}", self.addr);
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) { // 서버에서 읽어야 할 바이트 스트림, Read 트레이트의 read() 메서드에 대한 구체적인 구현체를 포함한다. 
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }, 
+                        Err(e) => {
+                            println!("Failed to read a buffer: {}", e);
+                        }
+                    }
+
+                },
+                Err(e) => {
+                    println!("Failed to establish a connection: {}", e); 
+                }
+            }
+        }
     }
 }
