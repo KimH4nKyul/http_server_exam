9845e814e841aea3b47daac4962e8eb2d36bc0a7
KimH4nKyul <alskim93@kakao.com>
2023-02-20 20:40:52 +0900

    [New] new feature added Basic custom error using Debug, Display, Error trait.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,7 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
 
 pub struct Request {
     path: String,
@@ -7,12 +9,51 @@ pub struct Request {
     method: Method
 }
 
-impl Request {
-    pub fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
-        Self
+// impl Request {
+//     pub fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+//         unimplemented!()
+//     }
+// }
+
+impl TryFrom<&[u8]> for Request {
+    // type Error = String; 
+    type Error = ParseError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }   
+}
+
+pub enum ParseError {
+    InvalidRequest, 
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "INVALID REQUEST", 
+            Self::InvalidEncoding => "INVALID ENCODING",
+            Self::InvalidProtocol => "INVALID PROTOCOL",
+            Self::InvalidMethod => "INVALID METHOD",
+        }
     }
 }
 
-impl TryFrom<&[u8]> for Request {
-    // TODO
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {
+        
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::net::TcpListener;
 use std::io::Read;  // stream에서 read() 메서드를 사용하기 위해 Read 트레이트를 불러온다. 
 
@@ -23,12 +26,23 @@ impl Server {
                     match stream.read(&mut buffer) { // 서버에서 읽어야 할 바이트 스트림, Read 트레이트의 read() 메서드에 대한 구체적인 구현체를 포함한다. 
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            // as 키워드를 통해 참조를 바이트 슬라이스로 취급할 수 있다. 
+                            // Request::try_from(&buffer as &[u8]); 
+                            // 아래 방식은 위 주석처리된 문장과 동일한 방식임. (직접 바이트 슬라이스로 생성해주는 방식)
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                        
+                                }, 
+                                Err(e) => {
+                                    println!("Faild to parse a request: {}", e);
+                                }
+                            }
+                            
                         }, 
                         Err(e) => {
                             println!("Failed to read a buffer: {}", e);
                         }
                     }
-
                 },
                 Err(e) => {
                     println!("Failed to establish a connection: {}", e); 
