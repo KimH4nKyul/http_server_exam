313a4b43651955699233f19f2a32433c57d420bb
KimH4nKyul <alskim93@kakao.com>
2023-02-20 14:34:51 +0900

    [Upadte] make modules.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use server::Server;
+use http::request::Request;
+
 fn main() {
     let addr = String::from("127.0.0.1:8080").to_string();
     let server = Server::new(addr);
@@ -5,37 +8,45 @@ fn main() {
 }
 
 mod server { 
-    struct Server {
+    pub struct Server {
         addr: String,
     }
     
     impl Server {
-        fn new(addr: String) -> Self {
+        pub fn new(addr: String) -> Self {
             Self {
                 addr
             }
         }
     
-        fn run(self) {
+        pub fn run(self) {
             println!("Connection: {}", self.addr);
         }
     }
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>, 
-    method: Method
-}
+mod http {
+    pub mod request {
+        use super::method::Method;
 
-enum Method {
-    GET,
-    POST,
-    DELTE,
-    PUT,
-    PATCH,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
+        pub struct Request {
+            path: String,
+            query_string: Option<String>, 
+            method: Method
+        }
+    }
+
+    mod method { 
+        pub enum Method {
+            GET,
+            POST,
+            DELTE,
+            PUT,
+            PATCH,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+        }
+    }
 }
