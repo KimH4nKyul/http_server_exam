fc004c4440e34e0bf6b4c053e9287ad73745d518
KimH4nKyul <alskim93@kakao.com>
2023-02-25 17:27:46 +0900

    [Update] Custom Error processing for utf8.

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -19,7 +21,31 @@ impl TryFrom<&[u8]> for Request {
     // type Error = String; 
     type Error = ParseError;
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+    // GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        /*
+        str::from_utf8(buf)
+        버퍼를 요청으로 변경하기 위해서는 버퍼 슬라이스를 문자열 슬라이스로 변경해야 한다.
+        str의 from_utf8은 버퍼 슬라이스를 문자열 슬라이스로 변경함과 동시에 utf8 형태인지 확인해 준다.
+         */
+
+        // 패턴 1. 
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {  }
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
+
+        // 패턴 2. 러스트에서 권장하는 패턴임.
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(request) => { unimplemented!() }
+        //     Err(e) => return Err(e), 
+        // }
+
+        // 패턴3. ? 연산자는 match 구문과 비슷한 일을 함. 
+        // ? 연산자는 반환할 오류와 매칭할 오류가 명시되어 있어야 함. 그렇지 않으면 필요한 트레이트를 직접 구현해줘야 함.
+        // let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+        let request = str::from_utf8(buf)?;
+
         unimplemented!()
     }   
 }
@@ -51,6 +77,12 @@ impl Display for ParseError {
 impl Debug for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
